e8589a730f6d60f414cd5a55711327046cd22da4
seo6413 <tjwldyd95@naver.com>
2018-07-24 09:05:18 +0000

    Style: spacebar

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -2,22 +2,16 @@ class ListController < ApplicationController
 
   def findOneStation
 
-
   end
   
   def findStores
-        #이 액션에서 검색을 수행한다
+    #이 액션에서 검색을 수행한다
     @station = Station.all
     @station =  Station.where("station_name like ?", params[:search])
     #where을해서 statioDB에 폼태그에 작석한 station_nmae과 같은 레코드들를(객체배열)을반환 한다. (레코드는 모델의 하나의 객체)
     
     @temp = Store.where(:station_id => @station[0].id)
-
     # @selectedstores = Store.where("station_name like ?", @station[0].station_name)
-
-  
   end
-  
-
   
 end
