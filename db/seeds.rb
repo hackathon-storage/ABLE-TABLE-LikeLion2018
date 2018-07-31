b198b42723fcc39edf2d453a64648ff2ff56bb7a
seojiyong <37373730+seo6413@users.noreply.github.com>
2018-07-31 17:37:52 +0900

    Feateure : 씨드파일 (#9)
    * Feat: 기능추가
    
    어떤기능인지 자세하게써봐
    
    * TEST
    
    * Feat:
    
    모델들을 만들고 DB에 컬럼들을 만들었습니다.
    
    * Feat: DB테이블 관계 설정
    
    댓글로 피드백 해주신 테블간의 관계를 설정 해주었습니다.
    
    * Feat: t.timestamps null: false수정
    
    t.timestamps null: false로 수정 해놨습니다.
    
    * Feat: 피드백 요청 처리
    
    피드백 요청으로 DB테이블들의 위치를 수정하고, timestamps에 null:false값을 추가 했습니다.
    
    * Feature : 검색기능 및 씨드생성
    
    검색기능 코드를 3줄로 줄이고 씨드 파일을 만들어서 초기데이터를 만들었습니다.><
    
    * Fix: remove schema.rb
    
    * Style: spacebar
    
    * Feature: 도로명 주소 등록 및 관계 설정
    
    도로명 주소를 등록하고 씨드파일 수정하고 관계를 설정해서 컨트롤러를 수정했습니다.
    
    * Feat: Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,85 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Station.create(station_name: '역곡역',station_position_x: '12.231',station_position_y: '93.21')
-Station.create(station_name: '온수역', station_position_x: '37.492338', station_position_y: '126.823656')
-Station.create(station_name: '부천역', station_position_x: '3663.231', station_position_y: '7334.21')
-Station.create(station_name: '구로역', station_position_x: '4663.231', station_position_y: '6334.21')
-Station.create(station_name: '강남역', station_position_x: '37.498176', station_position_y: '127.027578')
-Station.create(station_name: '철산역', station_position_x: '6663.231', station_position_y: '4334.21')
-Station.create(station_name: '광명사거리역', station_position_x: '6663.231', station_position_y: '3334.21')
-Station.create(station_name: '천왕역', station_position_x: '7663.231', station_position_y: '2334.21')
-Station.create(station_name: '가산디지털단지역', station_position_x: '8663.231', station_position_y: '1334.21')
+Station.create(station_name: '역곡역',station_position_x: 37.485156,station_position_y: 126.811622)
+Station.create(station_name: '온수역', station_position_x: 37.4921505, station_position_y: 126.8236345)
+Station.create(station_name: '부천역', station_position_x: 37.484083, station_position_y:  126.782776)
+Station.create(station_name: '구로역', station_position_x: 37.503195 , station_position_y: 126.882038)
 
+Store.create(user_id: '1',station_id: '1', store_name: '리얼프라이',store_address: '경기 부천시 괴안동 115-6')
+Store.create(user_id: '2',station_id: '1',store_name: '미쳐버린 파닭',store_address: '경기 부천시 역곡동 35-14')
+Store.create(user_id: '3',station_id: '1',store_name: '호치킨',store_address: '경기 부천시 괴안동 155-6')
+Store.create(user_id: '4',station_id: '1',store_name: '맛닭꼬',store_address: '	경기 부천시 역곡동 78-11')
 
+Store.create(user_id: '5',station_id: '2',store_name: '터프네 이모',store_address: '서울 구로구 온수동 55-4')
+Store.create(user_id: '6',station_id: '2',store_name: '호프할래',store_address: '서울 구로구 온수동 51-4')
+Store.create(user_id: '7',station_id: '2',store_name: '돈돈',store_address: '서울 구로구 온수동 55-4')
+Store.create(user_id: '8',station_id: '2',store_name: '바베큐 보스 치킨',store_address: '서울 구로구 온수동 52-1')
+Store.create(user_id: '9',station_id: '2',store_name: '학사마을',store_address: '서울 구로구 온수동 55-4')
+Store.create(user_id: '10',station_id: '2',store_name: '숯불닭과왕파전',store_address: '서울 구로구 온수동 53-4')
+Store.create(user_id: '11',station_id: '2',store_name: '봉구비어',store_address: '서울 구로구 온수동 52')
 
-Store.create(user_id: '1',station_id: '1', store_name: '오미치킨',store_address: '')
-Store.create(user_id: '2',station_id: '1',store_name: '미쳐버린 파닭',store_address: '')
-Store.create(user_id: '3',station_id: '1',store_name: '호치킨',store_address: '')
-Store.create(user_id: '4',station_id: '1',store_name: '맛닭꼬',store_address: '')
+Store.create(user_id: '12',station_id: '3',store_name: '유리네밥상',store_address: '경기 부천시 심곡동 377-12')
 
-Store.create(user_id: '5',station_id: '2',store_name: '터프네 이모',store_address: '경인로3길 86')
-Store.create(user_id: '6',station_id: '2',store_name: '호프할래',store_address: '경인로3길 69')
-Store.create(user_id: '7',station_id: '2',store_name: '돈돈',store_address: '구로구 경인로3길 86')
-Store.create(user_id: '8',station_id: '2',store_name: '바베큐 보스 치킨',store_address: '')
-Store.create(user_id: '9',station_id: '2',store_name: '학사마을',store_address: '')
-Store.create(user_id: '10',station_id: '2',store_name: '왕파전숯불닭',store_address: '')
-Store.create(user_id: '11',station_id: '2',store_name: '봉구비어',store_address: '')
-
-Store.create(user_id: '12',station_id: '3',store_name: '티바 두마리치킨',store_address: '')
-Store.create(user_id: '13',station_id: '3',store_name: '호식이 2마리 치킨',store_address: '')
-Store.create(user_id: '14',station_id: '3',store_name: '남국가')
-Store.create(user_id: '15',station_id: '4',store_name: '구로최고치킨집',store_address: '')
-Store.create(user_id: '16',station_id: '5',store_name: '빌리엔젤',store_address: '테헤란로1길 48')
-
-# Store.create(user_id: '3',: '역곡역',store_name: '호치킨',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')
-            
-# Store.create(user_id: '4',: '',store_name: '썬더치킨',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')            
-            
-# Store.create(user_id: '5',: '',store_name: '터프네 이모',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')    
-            
-# Store.create(user_id: '6',: '',store_name: '호프할래',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')
-            
-# Store.create(user_id: '7',: '',store_name: '학사마을',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')     
-            
-# Store.create(user_id: '8',: '',store_name: '돈돈',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')       
-            
-# Store.create(user_id: '9',: '',store_name: '봉구비어',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')
-
-# Store.create(user_id: '10',: '부평역',store_name: '레드락 치킨',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')
-      
-# Store.create(user_id: '10',: '부평역',store_name: '명랑 핫도그',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')
-    
-# Store.create(user_id: '11',: '부평역',store_name: '애슐리W',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')            
-
-# Store.create(user_id: '11',: '부천역',store_name: '유리네분식',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',storeimg: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')
-            
-# Store.create(user_id: '12',: '부천역',store_name: '티바 두마리 치킨',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')
-            
-# Store.create(user_id: '13',: '금정역',store_name: '남궁가',store_menu_img1: '',store_menu_img2: ''
-#             ,store_menu_img3: '',store_img: '',store_information: '',store_contact: '',store_group: ''
-#             ,store_seat_count: '',store_seat_total: '40',store_address: '')            
+Store.create(user_id: '13',station_id: '4',store_name: '호식이두마리치킨',store_address: '서울 구로구 구로4동 747-36')
